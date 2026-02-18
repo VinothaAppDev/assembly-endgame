549079320e16df6c23b4b5375adfb11ffe3cf032
VinothaAppDev <vinotha29022004@gmail.com>
2026-02-18 10:30:43 +0530

    added header and status bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,18 @@ import { useState } from 'react'
 import './App.css'
 
 
+
 export default function App(){
     return (
-        <>
-            <p>hello</p>
-        </>
+        <main>
+            <hgroup>
+                <h3>Assembly: Endgame</h3>
+                <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+            </hgroup>
+            <section className="status-bar">
+                <h3>You Win</h3>
+                <p>Well done! 🎉</p>
+            </section>
+        </main>
     )
 }
