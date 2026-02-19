8e3f38130d445a2ab99df17aeb7380470260ba8a
VinothaAppDev <vinotha29022004@gmail.com>
2026-02-19 17:26:25 +0530

    keyboard and word functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,49 @@ import { languages } from './languages'
 
 export default function App(){
 
-    const [ currentWord, setCurrentWord ] = useState("react");
+    const [ currentWord, setCurrentWord ] = useState("elephant");
 
     const letters = "abcdefghijklmnopqrstuvwxyz"
 
+    const [ guessedLetters, setGuessedLetters ] = useState("")
+
     const wordBox = Array.from(currentWord).map((letter, index) => {
+        const isGuessed = guessedLetters.includes(letter)
+
         return (
-            <span className='word-letter' key={index}>{letter.toUpperCase()}</span>
+            <span className='word-letter' key={index}>{isGuessed ? letter.toUpperCase() : ""}</span>
         )
     })
+    
+    console.log(guessedLetters)
+
+    const alphabetChosen = (letter) => {
+        setGuessedLetters((prev) => (
+            prev.includes(letter) ? prev : (prev+letter)
+        ))
+    }
 
     const keyboard = Array.from(letters).map(letter => {
+        const isGuessed = guessedLetters.includes(letter)
+
+        let letterStyles
+
+        if(isGuessed){
+            if(currentWord.includes(letter))
+                letterStyles = { backgroundColor: "#10A95B" }
+            else
+                letterStyles = { backgroundColor: "#EC5D49 "}
+        }
+
         return (
-            <button className='alphabet' key={letter}>{letter.toUpperCase()}</button>
+            <button 
+                style={letterStyles}
+                className='alphabet' 
+                key={letter} 
+                onClick={()=>alphabetChosen(letter)}
+            >
+                    {letter.toUpperCase()}
+            </button>
         )
     })
 
@@ -31,6 +61,10 @@ export default function App(){
             <div style={styles} key={obj.name}>{obj.name}</div>
         )
     })
+
+    function newGameCaller(){
+        // to do
+    }
 
     return (
         <main>
@@ -51,7 +85,9 @@ export default function App(){
             <section className="key-board">
                 {keyboard}
             </section>
-            <button className="new-game-btn">New Game</button>
+            <button className="new-game-btn" onClick={newGameCaller}>
+                New Game
+            </button>
         </main>
     )
 }
